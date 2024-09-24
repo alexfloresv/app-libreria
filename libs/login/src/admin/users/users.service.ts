7d22a9bcf1a582f063cbff65786c026395f27950
Alx <worfysito15@gmail.com>
2024-09-24 16:33:08 -0500

    segundo comit

--- libs/login/src/admin/users/users.service.ts
+++ libs/login/src/admin/users/users.service.ts
@@ -5,13 +5,13 @@ import {
   Logger,
   NotFoundException
 } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '@login/login/prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
 import { CreateUserDto, UpdateUserDto } from './dto';
-import { handleException } from 'src/utils';
+import { handleException } from '@login/login/utils';
 import { RolService } from '../rol/rol.service';
 import { generate } from 'generate-password';
-import { HttpResponse, Rol, UserData, UserPayload } from 'src/interfaces';
+import { HttpResponse, Rol, UserData, UserPayload } from '@login/login/interfaces';
 import { TypedEventEmitter } from 'src/event-emitter/typed-event-emitter.class';
 import { SendEmailDto } from './dto/send-email.dto';
 import { UpdatePasswordDto } from '../auth/dto/update-password.dto';
--- libs/login/src/login.module.ts
+++ libs/login/src/login.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { LoginService } from './login.service';
 import { AdminModule } from './admin/admin.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { InterfacesModule } from './interfaces/interfaces.module';
 
 @Module({
   providers: [LoginService],
   exports: [LoginService],
-  imports: [AdminModule, PrismaModule, InterfacesModule],
+  imports: [AdminModule, PrismaModule],
 })
 export class LoginModule {}
